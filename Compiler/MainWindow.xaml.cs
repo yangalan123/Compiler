662db420d77739e1c37c68a41561b864a6c66396
yangalan123 <1049909407@qq.com>
2017-11-21 18:58:34 +0800

    Init

--- Compiler/MainWindow.xaml.cs
+++ Compiler/MainWindow.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using Microsoft.Win32;
 namespace Compiler
 {
     /// <summary>
@@ -20,9 +20,45 @@ namespace Compiler
     /// </summary>
     public partial class MainWindow : Window
     {
+        String InputFileName = "";
+        String OutputFileName = "";
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //button for 'run'
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            //button for 'selecting input file'
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.RestoreDirectory = true;
+            dialog.Filter = "*.txt|*.*";
+            dialog.FilterIndex = 0;
+            if (dialog.ShowDialog() == true)
+            {
+                ((Button)sender).Content = "(点击可以再次选择)";
+                InputFileName = dialog.FileName;
+                this.InputFileDest.Text = InputFileName;
+            }
+        }
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            //button for 'selecting output file'
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.RestoreDirectory = true;
+            dialog.Filter = "*.txt|*.*";
+            dialog.FilterIndex = 0;
+            if (dialog.ShowDialog() == true)
+            {
+                ((Button)sender).Content = "(点击可以再次选择)";
+                OutputFileName = dialog.FileName;
+                this.OutputFileDest.Text = OutputFileName;
+            }
         }
     }
 }
